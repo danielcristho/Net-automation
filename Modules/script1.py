115cf094410eec694fff4a68ffb7bc88dd5dabb3
pepuhodaniel <pepuhodaniel93.learn@gmail.com>
2021-06-14 18:29:41 +0900

    belajar menggunakan modules

---
+++ Modules/script1.py
@@ -0,0 +1,8 @@
+def func1():
+    print("hi func1")
+
+def func2():
+    print("hi func2")    
+
+def my_data(name, city):
+    print(f"halo {name} from {city}")
---
+++ Modules/script2.py
@@ -0,0 +1,9 @@
+#import script1
+from script1 import my_data, func2
+print("this is script2")
+
+#script1.func1()
+#script1.func2()
+#script1.my_data("daniel", "jayapura")
+my_data("daniel", "jayapura")
+func2()
