81059d316a08c3d45bc87f55dd3430d2c00062e3
danielcristho <pepuhodaniel93.learn@gmail.com>
2021-07-25 13:06:33 +0900

    cara buat package di python

---
+++ Package1/module1.py
@@ -0,0 +1,8 @@
+def func1():
+    print("Mod1 from func1")
+
+def func2():
+    print("Mod1 from func2")
+
+def func3():
+    print("Mod1 from func3")   
---
+++ Package1/module2.py
@@ -0,0 +1,5 @@
+def func1():
+    print("Mod2 from func1")
+
+def func2():
+    print("Mod2 from func2")
