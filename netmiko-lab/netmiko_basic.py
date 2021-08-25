944e584ad40e1bea626cfd3c5baefce69704cffa
danielcristho <pepuhodaniel93.learn@gmail.com>
2021-08-25 20:50:46 +0900

    mikrotik_testing

--- netmiko-lab/netmiko_basic.py
+++ netmiko-lab/netmiko_basic.py
@@ -1,12 +1,24 @@
 from netmiko import ConnectHandler
 rtr1 = {
-    "device_type": "cisco_ios", 
+    "device_type": "mikrotik_routeros", 
     "host": "10.10.10.1",
-    "username": "cisco",
+    "username": "router1",
+    "password": "1234",
+}
+
+rtr2 = {
+    "device_type": "cisco_ios",
+    "host": "10.10.10.2" ,
+    "username": "router2" ,
     "password": "cisco",
 }
 
 conn = ConnectHandler(**rtr1)
-output = conn.send_command("show ip int brief | exclude unassigned")
+output = conn.send_config_set("ip address print")
+
+conn = ConnectHandler(**rtr2)
+output = conn.send_config_set("ip interface brief | exclude unassigned")
+
+print("------------------------------------")
 print(f"Interface on {rtr1['host']}")
 print(output)
