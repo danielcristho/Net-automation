c4c5f5604c43317d7aa2f06d8d4fd20b4c8501a2
daniel.pepuho <69733783+danielcristho@users.noreply.github.com>
2021-09-21 16:32:30 +0900

    Delete Package1 directory

--- Package1/module1.py
+++
@@ -1,8 +0,0 @@
-def func1():
-    print("Mod1 from func1")
-
-def func2():
-    print("Mod1 from func2")
-
-def func3():
-    print("Mod1 from func3")   
--- Package1/module2.py
+++
@@ -1,5 +0,0 @@
-def func1():
-    print("Mod2 from func1")
-
-def func2():
-    print("Mod2 from func2")
